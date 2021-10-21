f45b82027aae741bd6522598b8bc21369147d1c1
zhuyunfeng <i@zhuyunfeng.com>
2021-10-21 18:54:29 +0800

    feat

--- js/home.js
+++ js/home.js
@@ -1,1 +1,8 @@
 console.log('this is home page');
+document.addEventListener('hashchange', ()=>{
+  console.log('hashchange', location.href);
+})
+let id = 1;
+setInterval(()=>{
+  console.log('this is home', id++);
+}, 1000)
